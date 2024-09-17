c6d939a91a24815ea162e7d8acbc88ca585c9796
BuzoEmma <chibuzoemmanuel55@gmail.com>
2024-09-17 14:56:36 +0100

    made some adjustment on the pages

---
+++ my-react-portfolio/src/scenes/Projects.jsx
@@ -0,0 +1,103 @@
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title, description }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          {description}
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+        As a frontend developer, I enjoy transforming ideas into responsive, user-friendly applications. Below are some projects that showcase my expertise in JavaScript frameworks, HTML, CSS. These projects reflect my ability to build dynamic, functional, and visually appealing web applications using technologies like React, Tailwind CSS, and more.
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" description="Music recommender based on lists of liked songs"/>
+          <Project title="Project 2" description="An Online Educational Platform that alloows users to access courses, track their progress and interact with rheir instructors."/>
+
+          {/* ROW 2 */}
+          <Project title="Project 3" description="A simple Web Developement project plan"/>
+          <Project title="Project 4" description="A simple Rock Paper Scissors built using Javascript, HTML and CSS. "/>
+          <Project title="Project 5" description="An Admin Dashboard designed to manage user date, monitor system performance and genertae reports."/>
+
+          {/* ROW 3 */}
+          <Project title="Project 6" description="A Music Application landing page"/>
+          <Project title="Project 7" description="A responsive web application that provides users with realtime data visualization through interactive chart and table."/>
+
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
